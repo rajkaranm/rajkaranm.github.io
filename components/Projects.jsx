060ac83fc290b12e977ba017b3290c95db4b6106
rajkaranm <rajkaranvir456@gmail.com>
2023-12-26 17:55:45 +0530

    replaced all class with className

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -6,19 +6,19 @@ export default function Projects() {
       <h1 className="text-6xl font-bold text-center">PROJECTS</h1>
 
       <div
-        class="w-full sm:flex sm:flex-col sm:items-center md:flex md:flex-row md:justify-evenly xl:flex xl:flex-row my-28 xl:justify-evenly xl:items-center"
+        className="w-full sm:flex sm:flex-col sm:items-center md:flex md:flex-row md:justify-evenly xl:flex xl:flex-row my-28 xl:justify-evenly xl:items-center"
         data-aos="fade-up"
       >
         <Image
-          class="w-[30rem] h-[19rem] rounded-xl"
+          className="w-[30rem] h-[19rem] rounded-xl"
           src="https://raw.githubusercontent.com/rajkaranm/crypton/main/img/crypton.png"
           alt=""
           width="400"
           height="300"
         />
-        <div class="w-[20rem] m-10">
-          <h3 class="text-[2rem] font-bold">#1 Crypton</h3>
-          <p class="opacity-50">
+        <div className="w-[20rem] m-10">
+          <h3 className="text-[2rem] font-bold">#1 Crypton</h3>
+          <p className="opacity-50">
             It is integrated with Meta Mask. User can transfer ETH on to another
             user's Wallet on Etherium blockchain. User is provided with Tools
             page where user can view live price and live graph of the price of
@@ -33,19 +33,19 @@ export default function Projects() {
       </div>
 
       <div
-        class="w-full sm:flex sm:flex-col sm:items-center md:flex md:flex-row md:justify-evenly xl:flex xl:flex-row my-28 xl:justify-evenly xl:items-center"
+        className="w-full sm:flex sm:flex-col sm:items-center md:flex md:flex-row md:justify-evenly xl:flex xl:flex-row my-28 xl:justify-evenly xl:items-center"
         data-aos="fade-up"
       >
         <Image
-          class="w-[30rem] h-[19rem] rounded-xl"
+          className="w-[30rem] h-[19rem] rounded-xl"
           src="https://raw.githubusercontent.com/rajkaranm/scrpyard/main/img/scrpyard.png"
           alt=""
           width="400"
           height="300"
         />
-        <div class="w-[20rem] m-10">
-          <h3 class="text-[2rem] font-bold">#2 Scrpyard</h3>
-          <p class="opacity-50">
+        <div className="w-[20rem] m-10">
+          <h3 className="text-[2rem] font-bold">#2 Scrpyard</h3>
+          <p className="opacity-50">
             Scrpyard is E-Commerce website, It has feature to <b>share</b> your
             project and <b>buy</b> products of other users, It has{" "}
             <b>Cart feature</b>, It is also integrated with <b>Razorpay's</b>{" "}
@@ -60,19 +60,19 @@ export default function Projects() {
       </div>
 
       <div
-        class="w-full sm:flex sm:flex-col sm:items-center md:flex md:flex-row md:justify-evenly xl:flex xl:flex-row my-28 xl:justify-evenly xl:items-center"
+        className="w-full sm:flex sm:flex-col sm:items-center md:flex md:flex-row md:justify-evenly xl:flex xl:flex-row my-28 xl:justify-evenly xl:items-center"
         data-aos="fade-up"
       >
         <Image
-          class="w-[30rem] h-[19rem] rounded-xl"
+          className="w-[30rem] h-[19rem] rounded-xl"
           src="https://raw.githubusercontent.com/rajkaranm/share_source/main/github-assests/share_source.png"
           alt=""
           width="400"
           height="300"
         />
-        <div class="w-[20rem] m-10">
-          <h3 class="text-[2rem] font-bold">#3 Share Source</h3>
-          <p class="opacity-50">
+        <div className="w-[20rem] m-10">
+          <h3 className="text-[2rem] font-bold">#3 Share Source</h3>
+          <p className="opacity-50">
             Share Source is Social Media Forum were people can join various
             channels and post interesting thing, It is build using power of
             SvelteKit and PostgreSQL.
@@ -86,19 +86,19 @@ export default function Projects() {
       </div>
 
       <div
-        class="w-full sm:flex sm:flex-col sm:items-center md:flex md:flex-row md:justify-evenly xl:flex xl:flex-row my-28 xl:justify-evenly xl:items-center"
+        className="w-full sm:flex sm:flex-col sm:items-center md:flex md:flex-row md:justify-evenly xl:flex xl:flex-row my-28 xl:justify-evenly xl:items-center"
         data-aos="fade-up"
       >
         <Image
-          class="w-[30rem] h-[19rem] rounded-xl"
+          className="w-[30rem] h-[19rem] rounded-xl"
           src="https://raw.githubusercontent.com/Rajkaranm/spacehub/master/img/home.jpg"
           alt=""
           width="400"
           height="300"
         />
-        <div class="w-[20rem] m-10">
-          <h3 class="text-[2rem] font-bold">#4 Spacehub</h3>
-          <p class="opacity-50">
+        <div className="w-[20rem] m-10">
+          <h3 className="text-[2rem] font-bold">#4 Spacehub</h3>
+          <p className="opacity-50">
             It provide various information about space through <b>APIs</b> like
             NASA's Astronomy picture of the day and Various Other APIs. It is
             purely build with <b>Django</b>
